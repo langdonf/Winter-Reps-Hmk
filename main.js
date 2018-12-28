e77de069e2f96061ca174ecdc60ec6675b3a03d5
Langdon <lfroker@gmail.com>
2018-12-28 08:22:19 -0800

    section1

---
+++ main.js
@@ -0,0 +1,28 @@
+// ## I. Variables & Datatypes
+
+// ### A. Q + A
+
+// 1. How do we assign a value to a variable? 
+var variable1 = 'value1'
+console.log(variable1);
+// 2. How do we change the value of a variable? 
+var variable1 = 'value2'
+console.log(variable1);
+// 3. How do we assign an existing variable to a new variable?
+var variable2 = variable1
+console.log(variable2);
+// 4. Remind me, what are `declare`, `assign`, and `define`?
+
+'declare is to create a new variable name'
+
+'assign is when you give it a variable'
+
+'define is when you create a new variable and assign it a value '
+
+// 5. What is pseudocoding and why should you do it?
+'When you write out the solution to a problem by iusing english words instead of the language syntax. It helps you plan out what you are supposed to be doing without worring about that exact syntax to use.'
+
+// 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+'70/30?'
+
